cfa14b32ba4ec817e622a9c08be000c47a4e20ae
EmreG <emre.gurses@intertech.com.tr>
2017-09-25 21:53:43 +0300

    Initial Commit

---
+++ AppReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppReview
+//
+//  Created by EmreG on 25.09.2017.
+//  Copyright © 2017 EmreG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppReview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppReview
+//
+//  Created by EmreG on 25.09.2017.
+//  Copyright © 2017 EmreG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
